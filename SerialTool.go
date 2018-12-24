855fea12fe7cbfeec98968053606069686507bde
zhuxiping <julian.zhu@longsys.com>
2018-12-24 13:47:57 +0800

    change file suffix and folder

---
+++ SerialTool.go
@@ -0,0 +1,41 @@
+package main
+
+//go build -ldflags="-H windowsgui"  for hidden the cmd windows
+
+import (
+	"flag"
+	"fmt"
+	//	"math"
+	//	"os"
+	//	"path"
+	//	"strings"
+	//	"unicode/utf8"
+
+	"github.com/lxn/walk"
+	//	. "github.com/lxn/walk/declarative"
+	//	"github.com/lxn/win"
+	"github.com/serialgui"
+)
+
+var (
+	uiflag *string //environment parament
+	inTE   *walk.TextEdit
+
+//	mycanvas *walk.Canvas
+//	mytangle walk.Rectangle
+)
+
+type MyMainWindow struct {
+	*walk.MainWindow
+	tabWidget *walk.TabWidget
+	//	tabPage   *walk.TabPage
+	//	prevFilePath string
+}
+
+func main() {
+	uiflag = flag.String("_ui", "gui", "   <选择操作界面> [web] [gui] [cmd]")
+	flag.Parse()
+	fmt.Println(*uiflag)
+	serialgui.SerialWindow()
+	fmt.Println("maiwindow test")
+}
